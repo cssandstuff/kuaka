623a9b16807045b0efc2201728d1410b562152a1
Tim Clulow <tim@cssandstuff.com>
2023-03-11 21:44:42 +1300

    maybe

--- .tina/config.js
+++ .tina/config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "tinacms";
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
-  branch,
+  branch: "master",
   clientId: "b0cbd30d-c318-480b-ae05-b45d562d7b0f", // Get this from tina.io
   token: "1119a786bc854f860520aad43e2e50ffda483c89", // Get this from tina.io
   build: {
