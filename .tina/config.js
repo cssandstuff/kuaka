df63b872e0b213ba44824c675e76843f4e571c93
Tim Clulow <tim@cssandstuff.com>
2023-03-11 19:36:25 +1300

    tina

---
+++ .tina/config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "09deeaa7-e136-40dc-8e72-90893f7e5957", // Get this from tina.io
+  token: "c483fdbadd10ea04facb3ee2e8a8303388d38ce2", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        label: "Pages",
+        name: "pages",
+        path: "content",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
